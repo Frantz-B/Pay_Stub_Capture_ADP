84c8327c15e82deb0397eaada974ed5a77e366a5
Frantz <Frantz-b@github.com>
2016-11-13 04:15:55 -0500

    Figured out that just I actually did change the dir before but printed old value.  Added webdriver and unitest

--- GrabbingPayStubs.py
+++ GrabbingPayStubs.py
@@ -1,10 +1,26 @@
-import os
+import os as Forge
+from selenium import webdriver
+import time
+import unittest
 
-class GrabbingPayStubs():
+
+
+class GrabbingPayStubs(unittest.TestCase):
+
+    chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
+    adpURL = 'https://online.adp.com/portal/login.html'
 
     shit = 3
-    ooosht = os.getcwd()
+    ooosht = Forge.getcwd()
     print('this is the directory ', ooosht)
     downloadFilePath = '/Users/NewUser/Documents/Career'
-    downloadPath = os.chdir(downloadFilePath)
+    Forge.chdir(downloadFilePath)
+    downloadPath = Forge.getcwd()
     print(downloadPath)
+
+    def setUp(self):
+        chromeOptions = webdriver.ChromeOptions()
+        prefs = {'download.default_directory' : downloadFilePath}
+        chromeOptions.add_experimental_option('prefs', prefs)
+        self.cdriver = webdriver.Chrome(executable_path=chromeDriverLoc, chrome_options=chromeOptions)
+        self.cdriver.get(adpURL)
