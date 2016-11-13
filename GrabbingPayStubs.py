4c70056b8c6e45044790a3a08db1f83be8abcf25
New User <NewUser@owners-MBP.home>
2016-11-13 03:12:47 -0500

    2 trying to do naviagation.  Thinking i'm going to need to do OOP

--- GrabbingPayStubs.py
+++ GrabbingPayStubs.py
@@ -5,3 +5,6 @@ class GrabbingPayStubs():
     shit = 3
     ooosht = os.getcwd()
     print('this is the directory ', ooosht)
+    downloadFilePath = '/Users/NewUser/Documents/Career'
+    downloadPath = os.chdir(downloadFilePath)
+    print(downloadPath)
