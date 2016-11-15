df498c738126deb9f10234543e2c6a084390e49c
Frantz <Frantz-b@github.com>
2016-11-14 23:12:41 -0500

    Finally got all stubs to downlaod.  But new that an error would rise giving a variable something that is not there..  But it's cleaner to give it a name.  Guess I can turn that into a function as well

--- GrabbingPayStubs.py
+++ GrabbingPayStubs.py
@@ -13,13 +13,11 @@ class GrabbingPayStubs(unittest.TestCase):
     adpURL = 'https://online.adp.com/portal/login.html'
     userName = 'FBazile@Innotech1'
     passWord = 'red4red4'
-    #pysb = '#menuHit\2e node2'
-    #shit = 3
 
     ooosht = Forge.getcwd()
     print('this is the directory ', ooosht)
 
-    downloadFilePath = '/Users/NewUser/Documents/Career'
+    downloadFilePath = '/Users/NewUser/Documents/Career/2'
     Forge.chdir(downloadFilePath)
 
     downloadPath = Forge.getcwd()
@@ -27,7 +25,8 @@ class GrabbingPayStubs(unittest.TestCase):
 
     def setUp(self):
         chromeOptions = webdriver.ChromeOptions()
-        prefs = {'plugins.plugins_list': [{'enabled':False, 'name': 'Chrome PDF Viewer'}], 'download.default_directory' : self.downloadFilePath}
+        prefs = {'plugins.plugins_list': [{'enabled':False, 'name': 'Chrome PDF Viewer'}],
+                 'download.default_directory' : self.downloadFilePath}
         chromeOptions.add_experimental_option('prefs', prefs)
         self.cdriver = webdriver.Chrome(executable_path=self.chromeDriverLoc, chrome_options=chromeOptions)
         self.cdriver.get(self.adpURL)
@@ -39,41 +38,32 @@ class GrabbingPayStubs(unittest.TestCase):
         login.send_keys(self.userName)
         passField.send_keys(self.passWord + Keys.RETURN)
 
-        #time.sleep(4)
         self.cdriver.find_element_by_id('menuHit.node2').click()
 
-        time.sleep(2)
-        #self.cdriver.find_element_by_link_text('Pay Sta')
+        #time.sleep(2)
         self.cdriver.find_element_by_partial_link_text('Pay Statement').click()
 
-        #self.cdriver.find_element_by_css_selector('#dropMenu>table>tbody>tr:nth-child(2)>td>nobr>a').click()
-        #select.select_by_visible_text('Pay Statment')
-        # time.sleep(10)
-        #self.cdriver.find_element_by_partial_link_text('2016').click()
         time.sleep(4)
-        foxtrot = self.cdriver.find_element_by_xpath('//*[@title="Click to display the next page of results"]')
-        while foxtrot.is_displayed():
+        #foxtrot = self.cdriver.find_element_by_xpath('//*[@title="Click to display the next page of results"]')
+
+        whenToStop = True
+        while whenToStop:
+
+            #Grab 1st column names
             whiskey2 = []
             whiskey = self.cdriver.find_elements_by_partial_link_text('201')
             for webElement in whiskey:
                 whiskey2.append(webElement.text)
 
-        #main_chrome = self.cdriver.current_window_handle
+            #iterating thru list of pay statements and grabbing pdfs
+            for lnk in whiskey2:
 
-            for lnks in whiskey2:
-
-                pdf = lnks.replace('/', '.')
+                #pdf name capture & click on link
+                pdf = lnk.replace('/', '.')
                 pdfName = (pdf + '.pdf')
-                self.cdriver.find_element_by_link_text(lnks).click()
+                self.cdriver.find_element_by_link_text(lnk).click()
 
-            #lnks.send_keys(Keys.COMMAND + Keys.ENTER)
-            #self.cdriver.execute_script("window.open('www.google.com');", '_blank')
-            #self.cdriver.execute_script('''window.open("about:blank", "_blank");''')
-                time.sleep(2)
-            #self.cdriver.get(lnks.get_attribute('href'))
-            #lnks.send_keys(Keys.CONTROL + Keys.TAB)
-            #self.cdriver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + Keys.NUMPAD2)
-            #time.sleep(16)
+                time.sleep(4)
 
                 tango = self.cdriver.find_element_by_id('embedPdf').get_attribute('src')
                 self.cdriver.get(tango)
@@ -81,22 +71,21 @@ class GrabbingPayStubs(unittest.TestCase):
                 Forge.rename('download.pdf', pdfName)
                 time.sleep(4)
 
-            #self.cdriver.close()
-            #time.sleep(6)
-            #self.cdriver.refresh()
                 self.cdriver.find_element_by_id('cancelTopCenter').click()
                 time.sleep(6)
+
+            #clicking to next page of results & refreshing element of page
             foxtrot = self.cdriver.find_element_by_xpath('//*[@title="Click to display the next page of results"]')
-            foxtrot.click()
-            time.sleep(4)
-            foxtrot = self.cdriver.find_element_by_xpath('//*[@title="Click to display the next page of results"]')
-        #self.cdriver.find_element_by_css_selector('#icon').click()
-        time.sleep(10)
-        #tango = self.cdriver.find_element_by_id('embedPdf').get_attribute('src')
-        #self.cdriver.get(tango)
+            if foxtrot.is_displayed():
+                foxtrot.click()
+                time.sleep(6)
+            else: whenToStop = False
+            #newBlade = self.cdriver.find_element_by_xpath('//*[@title="Click to display the next page of results"]').is_displayed()
+                #if not newBlade:
+
+        #time.sleep(10)
 
         self.assertTrue(True, 'huh')
 
     def tearDown(self):
-        #Forge.rename('download.pdf', 'nuts.pdf')
         print(self.downloadPath)
